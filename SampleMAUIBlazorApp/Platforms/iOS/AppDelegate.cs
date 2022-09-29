c73c9f07830c63c517460861b2885134cd0dbe3d
Justin Britt <britt@uwindsor.ca>
2022-09-29 05:58:03 -0400

    Add project files.

---
+++ SampleMAUIBlazorApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace SampleMAUIBlazorApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
